e4b14b018a8f420a62d7ca3ab44576f3c1215d41
degroen <degroen@mail.ru>
2019-09-22 13:15:51 +0300

    hw_02.1

--- hw_02.1.c
+++ hw_02.1.c
@@ -2,5 +2,27 @@
 #include <math.h>
 
 int main(){
-	
+	double a, b, c;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	double D = b*b - 4*a*c;
+
+	if (a==0){
+		printf("Ошибка: не квадратное уравнение\n");
+		return 1;
+	}
+
+	if (D<0){
+		printf("Нет корней\n");
+	}
+	if (D==0){
+		double x;
+		x = -b/2/a;
+		printf("Один корень x = %f\n", x);
+	}
+	if (D>0){
+		double x1, x2;
+		x1 = (-b-sqrt(D))/2/a;
+		x2 = (-b+sqrt(D))/2/a;
+		printf("Два корня x1 = %f, x2 = %f\n", x1, x2);
+	}
 }
